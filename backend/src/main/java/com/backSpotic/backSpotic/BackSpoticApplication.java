4c5f95958e2fdbe016de6b3a3f45e0f95c6e1113
HwangVi <hwangvi216@gmail.com>
2026-05-19 14:16:51 +0200

    FIX: cambio para Using generated security password

--- backend/src/main/java/com/backSpotic/backSpotic/BackSpoticApplication.java
+++ backend/src/main/java/com/backSpotic/backSpotic/BackSpoticApplication.java
@@ -2,9 +2,10 @@ package com.backSpotic.backSpotic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.backSpotic.backSpotic")
 public class BackSpoticApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackSpoticApplication.class, args);
